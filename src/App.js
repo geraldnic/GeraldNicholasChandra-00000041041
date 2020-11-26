f961d3eb97fb3b9a4a5e7c4a030d1e7d8a95b4a2
geraldnic <gerald.nicholas@student.umn.ac.id>
2020-11-26 16:25:23 +0700

    quiz

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main text-center">
+      <div className="judul">SIMPLE COUNTDOWN</div>
+      <div className="info" id="info"></div>
+      <div className="up" id="up">
+      <svg width="50px" height="50px" viewBox="0 0 16 16" class="btn-jam text-left bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+</svg>
+<svg width="50px" height="50px" viewBox="0 0 16 16" class="btn-menit text-left bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+</svg>
+<svg width="50px" height="50px" viewBox="0 0 16 16" class="btn-detik text-left bi bi-caret-up-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path d="M7.247 4.86l-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+</svg>
+      </div>
     </div>
   );
 }
